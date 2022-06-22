6d9f7d9a78c8846b7c81e920be05934430984d0c
Magbo Charles <magboelochukwu@gmail.com>
2022-06-22 10:27:13 +0100

    second commit

--- I4G004124JJF/blog/views.py
+++ I4G004124JJF/blog/views.py
@@ -7,15 +7,18 @@ class PostListView(generic.ListView):
 
 class PostCreateView(generic.CreateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
 class  PostDetailView(generic.DetailView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+
+class  PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
 class  PostDeleteView(generic.DeleteView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
